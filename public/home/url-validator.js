9f5567af36de415d264e57add5c7ce1c3460ce03
German <germonram@gmail.com>
2022-02-11 09:04:17 -0600

    Pruebas de blockeo

--- public/home/url-validator.js
+++ public/home/url-validator.js
@@ -3,5 +3,13 @@ function validateLogin() {
 	let user = sessionStorage.getItem('glbvalor');
 	if (!user) {
 		window.location.replace('../index.html');
+	} else {
+		window.onbeforeunload = blockNavigation;
 	}
+}
+function blockNavigation(e) {
+	window.location.href = window.history.previous.href;
+	console.log(window.history.previous.href);
+
+	//Fuente: https://www.iteramos.com/pregunta/18414/como-se-obtiene-la-url-anterior-en-javascript
 }
