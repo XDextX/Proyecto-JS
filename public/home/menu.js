2bab62f8f33f18e402e19124d659a24056475379
German <germonram@gmail.com>
2022-03-30 21:37:57 -0600

    Arreglos de union

--- public/home/menu.js
+++
@@ -1,8 +0,0 @@
-
-
-function mostrar(){
-	var html = document.querySelector("link[rel='import']").import;
-	var text = html.getElementById("pt");
-
-document.body.appendChild(text.cloneNode(true));
-}
